964cfd567e465817594f15ebe7f339ead501b2b1
truongtran19315 <truong.chiptttran@gmail.com>
2023-07-09 11:50:13 +0700

    update schedule

--- Week3/scheduler.py
+++ Week3/scheduler.py
@@ -7,14 +7,14 @@ class Task:
     pTask = None
     Delay = 0
     Period = 0
-    RunMe = 0
+    RunMe = 0                   # set state of sched: 1 -> 0 -> Run pTask (pTask is a pointer function)
     TaskID = -1
 
 class Scheduler:
-    TICK = 100
-    SCH_MAX_TASKS = 40
-    SCH_tasks_G = []
-    current_index_task = 0
+    TICK = 100                  
+    SCH_MAX_TASKS = 40          
+    SCH_tasks_G = []            # List task (type list)
+    current_index_task = 0      # total number task current 
 
     def __int__(self):
         return
@@ -38,15 +38,30 @@ class Scheduler:
             else:
                 self.SCH_tasks_G[i].Delay = self.SCH_tasks_G[i].Period
                 self.SCH_tasks_G[i].RunMe += 1
+                    
 
     def SCH_Dispatch_Tasks(self):
+        deleteArr=[]
         for i in range(0, len(self.SCH_tasks_G)):
             if self.SCH_tasks_G[i].RunMe > 0:
                 self.SCH_tasks_G[i].RunMe -= 1
                 self.SCH_tasks_G[i].pTask()
+                if self.SCH_tasks_G[i].Period == 0 :
+                    deleteArr.append(self.SCH_tasks_G[i]);
+                    # self.SCH_Delete(self.SCH_tasks_G[i])
+                    # self.SCH_Dispatch_Tasks()
+                    # break        
+        for i in range(0,len(deleteArr)):
+            self.SCH_Delete(deleteArr[i])
 
     def SCH_Delete(self, aTask):
-        return
+        if aTask in self.SCH_tasks_G:
+            self.SCH_tasks_G.remove(aTask)
+            self.current_index_task -= 1    
 
     def SCH_GenerateID(self):
         return -1
+
+    # def SCH_Print(self):
+    #     for i in range(0, len(self.SCH_tasks_G)):
+    #         print(self.SCH_tasks_G[i].TaskID)
