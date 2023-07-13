dd099c7625ea1f133916e907a9e25f56d4eb571d
NgoQuangHai19 <quanghairmfc@gmail.com>
2023-07-13 14:09:16 +0700

    commit

--- Week3/physical.py
+++ Week3/physical.py
@@ -4,25 +4,25 @@ print("Sensors and Actuators")
 import serial.tools.list_ports
 
 
-# def getPort():
-#     ports = serial.tools.list_ports.comports()
-#     N = len(ports)
-#     commPort = "None"
-#     for i in range(0, N):
-#         port = ports[i]
-#         strPort = str(port)
-#         print(strPort) # print(strPort)
-#         if "FT232R USB UART" in strPort:
-#             splitPort = strPort.split(" ")
-#             print("PortName:",strPort)
-#             commPort = splitPort[0]
-#     return commPort
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        print(strPort) # print(strPort)
+        if "FT232R USB UART" in strPort:
+            splitPort = strPort.split(" ")
+            print("PortName:",strPort)
+            commPort = splitPort[0]
+    return commPort
 
 
-# portName = getPort()
-# print("portName:",portName)
-# if portName != "None":
-ser = serial.Serial(port="COM8", baudrate=9600)
+portName = getPort()
+print("portName:",portName)
+if portName != "None":
+    ser = serial.Serial(port=portName, baudrate=9600)
 
 relay1_ON = [0, 6, 0, 0, 0, 255, 200, 91]
 relay1_OFF = [0, 6, 0, 0, 0, 0, 136, 27]
