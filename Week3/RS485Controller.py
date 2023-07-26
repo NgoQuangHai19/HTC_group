a3667833c79ac437b2a1c88d64a3d8328b7d240b
truongtran19315 <truong.chiptttran@gmail.com>
2023-07-26 11:04:59 +0700

    update test RS485

--- Week3/RS485Controller.py
+++ Week3/RS485Controller.py
@@ -10,7 +10,7 @@ class RS485Controller:
         if bytesToRead > 0:
             out = self.ser.read(bytesToRead)
             data_array = [b for b in out]
-            print(data_array)
+            # print(data_array)
             if len(data_array) >= 7:
                 array_size = len(data_array)
                 value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
