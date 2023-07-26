2bfde37523a96d49c393d3d03ec12cbc9622f74e
truongtran19315 <truong.chiptttran@gmail.com>
2023-07-26 11:02:17 +0700

    update test RS485

--- Week3/testRS485.py
+++ Week3/testRS485.py
@@ -3,14 +3,14 @@ from RS485Controller import *
 import time
 
 controller = RS485Controller()
-
+controller.relayController(1, state=1)
+time.sleep(1)
+controller.relayController(1, state=0)
+time.sleep(1)
 while True:
-    controller.relayController(1, state=1)
-    time.sleep(1)
-    controller.relayController(1, state=0)
-    time.sleep(1)
     distance_9 = controller.getvalueDistance(9)
     print(f"Distance from sensor 9: {distance_9}")
     time.sleep(1.5)
     distance_12 = controller.getvalueDistance(12)
     print(f"Distance from sensor 12: {distance_12}")
+    time.sleep(1.5)
