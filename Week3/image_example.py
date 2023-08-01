52d975715ae5258779ef96c519aea92cef701668
NgoQuangHai19 <quanghairmfc@gmail.com>
2023-08-01 13:04:07 +0700

    add UI

--- Week3/image_example.py
+++ Week3/image_example.py
@@ -27,7 +27,7 @@ class App(customtkinter.CTk):
         # load images with light and dark mode image
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "hcmut.png")), size=(40, 40))
-        self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image2.png")), size=(150, 67))
+        self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image2.png")), size=(125, 50))
         self.image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
         self.home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
                                                  dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(20, 20))
@@ -43,7 +43,7 @@ class App(customtkinter.CTk):
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text=" Smart Agriculture", image=self.logo_image,
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text=" Smart Agriculture  ", image=self.logo_image,
                                                              compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=0, pady=0)
 
@@ -57,10 +57,10 @@ class App(customtkinter.CTk):
                                                       image=self.chat_image, anchor="w", command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
 
-        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 3",
-                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
-        self.frame_3_button.grid(row=3, column=0, sticky="ew")
+        # self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 3",
+        #                                               fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+        #                                               image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
+        # self.frame_3_button.grid(row=3, column=0, sticky="ew")
 
         self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
                                                                 command=self.change_appearance_mode_event)
@@ -79,52 +79,52 @@ class App(customtkinter.CTk):
             self.on_button.append(customtkinter.CTkButton(self.home_frame))
 
         self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="", image=self.large_test_image)
-        self.home_frame_large_image_label.grid(row=0, column=1, padx=0, pady=10)
+        self.home_frame_large_image_label.grid(row=0, column=1, padx=0, pady=10, sticky="n")
 
         # Tao cac buton
         self.on_button[0] = customtkinter.CTkButton(self.home_frame, text="Relay 1", image=self.on, compound="right", width=150, height=50,
                                                  command=lambda :self.toggle_button_click(1))
-        self.on_button[0].grid(row=6, column=0, padx=50, pady=15)
+        self.on_button[0].grid(row=2, column=0, padx=50, pady=15)
 
         self.on_button[1] = customtkinter.CTkButton(self.home_frame, text="Relay 2", image=self.on, compound="right", width=150, height=50,
                                                  command=lambda :self.toggle_button_click(2))
-        self.on_button[1].grid(row=6, column=1, padx=0, pady=15)
+        self.on_button[1].grid(row=2, column=1, padx=0, pady=15)
 
         self.on_button[2] = customtkinter.CTkButton(self.home_frame, text="Relay 3", image=self.on, compound="right", width=150, height=50,
                                                  command=lambda :self.toggle_button_click(3))
-        self.on_button[2].grid(row=6, column=2, padx=50, pady=15)
+        self.on_button[2].grid(row=2, column=2, padx=50, pady=15)
 
         self.on_button[3] = customtkinter.CTkButton(self.home_frame, text="Relay 4", image=self.on, compound="right", width=150, height=50,
                                                  command=lambda :self.toggle_button_click(4))
-        self.on_button[3].grid(row=7, column=0, padx=50, pady=15)
+        self.on_button[3].grid(row=3, column=0, padx=50, pady=15)
 
         self.on_button[4] = customtkinter.CTkButton(self.home_frame, text="Relay 5", image=self.on, compound="right", width=150, height=50,
                                                  command=lambda :self.toggle_button_click(5))
-        self.on_button[4].grid(row=7, column=1, padx=0, pady=15)
+        self.on_button[4].grid(row=3, column=1, padx=0, pady=15)
 
         self.on_button[5] = customtkinter.CTkButton(self.home_frame, text="Relay 6", image=self.on, compound="right", width=150, height=50,
                                                  command=lambda :self.toggle_button_click(6))
-        self.on_button[5].grid(row=7, column=2, padx=50, pady=15)
+        self.on_button[5].grid(row=3, column=2, padx=50, pady=15)
 
         self.on_button[6] = customtkinter.CTkButton(self.home_frame, text="Pump1", image=self.on, compound="right", width=150, height=50,
                                                  command=lambda :self.toggle_button_click(7))
-        self.on_button[6].grid(row=8, column=0, padx=80, pady=15)
+        self.on_button[6].grid(row=4, column=0, padx=80, pady=15)
 
 
         self.on_button[7] = customtkinter.CTkButton(self.home_frame, text="Pump2", image=self.on, compound="right", width=150, height=50,
                                                  command=lambda :self.toggle_button_click(8))
-        self.on_button[7].grid(row=8, column=2, padx=80, pady=15)
+        self.on_button[7].grid(row=4, column=2, padx=80, pady=15)
 
         # Hien thi muc nuoc
         self.distance1 = customtkinter.CTkProgressBar(self.home_frame, orientation="vertical", corner_radius=0, width = 85)
-        self.distance1.grid(row = 4, column=0)
+        self.distance1.grid(row = 0, column=0, pady=20)
         self.lableDistance1= customtkinter.CTkLabel(self.home_frame, text= "Volume of liquid 1", fg_color= "transparent")
-        self.lableDistance1.grid(row=5, column= 0, pady=0)
+        self.lableDistance1.grid(row=1, column= 0, pady=0)
 
         self.distance2 = customtkinter.CTkProgressBar(self.home_frame, orientation="vertical", corner_radius=0, width = 85)
-        self.distance2.grid(row = 4, column=2 )
+        self.distance2.grid(row = 0, column=2, pady=20)
         self.lableDistance2= customtkinter.CTkLabel(self.home_frame, text= "Volume of liquid 2", fg_color= "transparent", )
-        self.lableDistance2.grid(row=5, column= 2, pady=0)
+        self.lableDistance2.grid(row=1, column= 2, pady=0)
 
         
 
@@ -285,7 +285,7 @@ class App(customtkinter.CTk):
         # set button color for selected button
         self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
         self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
-        self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+        # self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
 
         # show selected frame
         if name == "home":
@@ -355,8 +355,8 @@ class App(customtkinter.CTk):
     def frame_2_button_event(self):
         self.select_frame_by_name("frame_2")
 
-    def frame_3_button_event(self):
-        self.select_frame_by_name("frame_3")
+    # def frame_3_button_event(self):
+    #     self.select_frame_by_name("frame_3")
 
     def change_appearance_mode_event(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -365,8 +365,7 @@ class App(customtkinter.CTk):
 # app=App(ser)
 # app.mainloop()
 app=App(None)
-while(True): 
-    app.mainloop()
+app.mainloop()
     #time.sleep(0.1)
         
         
